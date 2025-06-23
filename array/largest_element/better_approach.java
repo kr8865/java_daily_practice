1ae8060b92155f3ed5ab25bb4c0765fefcdde087
kr8865 <misskratikaagrawal@gmail.com>
2025-06-23 14:25:21 +0530

    array

---
+++ array/largest_element/better_approach.java
@@ -0,0 +1,17 @@
+public class better_approach {
+    public static void main(String[] args) {
+        // step-1 initialize a variable name max store the minimum value in it
+        // or you can consider the first value of array as max value
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int max = arr[0];
+        // traverse the array and compare it with max and update the variable
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.print(max);
+
+    }
+
+}
---
+++ array/largest_element/brute_forceapp.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class brute_forceapp {
+    public static void main(String[] args) {
+        // step-1 sort the array
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+        Arrays.sort(arr);
+        // step-2 return the last element of array
+        System.out.print("largest element is " + arr[arr.length - 1]);
+
+    }// time complexity---o(nlogn)
+
+}
